435be3dcd25225146e1739f9af61eef385f11fc7
Bhuvana-295556 <82052062+Bhuvana-295556@users.noreply.github.com>
2021-09-23 01:13:21 +0530

    Add files via upload

---
+++ ProdBL.cs
@@ -0,0 +1,20 @@
+﻿using ProductDAL;
+using ProductDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductBL
+{
+    public class ProdBL
+    {
+        ProdDAL DALObj = new ProdDAL();
+        public List<ProdDTO> GetProduct()
+        {
+            return DALObj.FetchDetails();
+        }
+
+    }
+}
---
+++ ProductDTO/ProdDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductDTO
+{
+    public class ProdDTO
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string ProductNumber { get; set; }
+        public bool MakeFlag { get; set; }
+        public bool FinishedGoodsFlag { get; set; }
+        public string Color { get; set; }
+        public short SafetyStockLevel { get; set; }
+        public short ReorderPoint { get; set; }
+        public decimal StandardCost { get; set; }
+        public decimal ListPrice { get; set; }
+        public string Size { get; set; }
+    }
+}
