91350ce4ecff07f475aa358021dc715a06496bd2
Bhuvana-295556 <82052062+Bhuvana-295556@users.noreply.github.com>
2021-09-23 01:15:36 +0530

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using ProductBL;
+using ProductDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductAPI.Controllers
+{
+    public class ProductController : ApiController
+    {
+
+        ProdBL blObj;
+
+        public List<ProdDTO> GetProduct()
+        {
+            blObj = new ProdBL();
+            var result = blObj.GetProduct();
+            return (List<ProdDTO>)result;
+        }
+    }
+}
